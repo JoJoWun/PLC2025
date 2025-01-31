ebb22f4b5892e321a71c3cb70b8e2067b42dd525
Jordan  Oni <Jordan.oni444@gmail.com>
2025-01-31 21:29:35 +0000

    task 1-3

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,14 @@
+module Main where
+
+--  the onePlusone function OUTSIDE of main
+onePlusone :: IO ()
+onePlusone = do
+  putStrLn "1 + 1 = 2"
+
+-- Main function
+main :: IO ()
 main = do
-    putStrLn "Welcome to the programme. Please enter your name"
-    name <- getLine
-    putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+  onePlusone  -- Call the onePlusone function
+  putStrLn "Welcome to the programme. Please enter your name"
+  name <- getLine
+  putStrLn ("Hello " ++ name ++ ", hope you like Haskell.")
--- lab2-functionalprimer/haskell/task3.hs
+++ lab2-functionalprimer/haskell/task3.hs
@@ -1,7 +1,15 @@
-sgn x = if x < 0 then -1 else if x == 0 then 0 else 1
+module Main where
 
+-- Define the sgn function using guards
+sgn x
+  | x < 0     = -1
+  | x == 0    = 0
+  | otherwise = 1
+
+-- Main function
+main :: IO ()
 main = do
-    putStrLn "Please enter a number"
-    input <- getLine 
-    let x = (read input :: Int)
-    putStrLn( show (sgn (x)) )
+  putStrLn "Please enter a number"
+  input <- getLine
+  let x = (read input :: Int)  -- Convert input to an integer
+  putStrLn (show (sgn x))      -- Print the result of sgn
