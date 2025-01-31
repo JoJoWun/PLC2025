155518f982546e4b350ab16ebdd37c26a98c3573
Jordan  Oni <Jordan.oni444@gmail.com>
2025-01-31 21:40:20 +0000

    task 4

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -1,14 +1,20 @@
+module Main where
+
+-- Define the ask function
 ask :: String -> IO ()
 ask prompt =
   do
-  putStrLn prompt
-  line <- getLine
-  if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+    putStrLn prompt
+    line <- getLine
+    if line == "quit"
+      then putStrLn "quitting..."
+      else if line == ""
+        then ask (prompt ++ "/")  -- Add a "/" to the prompt
+        else putStrLn ("you said: " ++ reverse line)
 
+-- Main function
 main :: IO ()
 main =
   do
-  let prompt = "please say something"
-  ask prompt
+    let prompt = "please say something"
+    ask prompt
